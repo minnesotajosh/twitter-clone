b37fc02db94e804a1049e566bba036fc1ceb9931
Josh Longanecker <jlonganecker@lifetouch.com>
2017-09-01 22:09:12 -0500

    =Refactor app

---
+++ client/createTweetContainer/createTweetContainer.js
@@ -0,0 +1,19 @@
+Template.createTweetContainer.events({
+    'submit form': (event) => {
+        event.preventDefault();
+
+        let formArray = $(event.target).serializeArray();
+        let tweet = {};
+
+        for (let i = 0; i < formArray.length; i++) {
+            tweet[formArray[i].name] = formArray[i].value;
+        }
+
+        //since tweet-box is contenteditable div instead of form element:
+        let tweetBox = $(event.target).find('[data="tweet-box"]');
+        tweet.tweetData = tweetBox.html();
+        Meteor.call('insertTweet', tweet);
+        event.target.reset();
+        tweetBox.html('');
+    }
+});
